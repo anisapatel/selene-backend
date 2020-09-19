7dee206f9c80e2f29c68395fc934a4db579f16e1
anisapatel <anisa_patel@live.co.uk>
2020-09-19 21:58:46 +0100

    added an instance of mongodb

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const apiRouter = require("./routes/apiRouter");
 
 require("dotenv").config();
 const bodyParser = require("body-parser"); //retrieve values from req.body otherwise it is undefined
+const session = require("express-session"); //Require the session for saving user data and giving a user a unique experience.
 const cors = require("cors");
+const db = require("./db/data/index");
 
 const adminController = require("./controllers/adminController");
 const cloudinaryController = require("./controllers/cloudinaryController");
 const productsController = require("./controllers/productsController");
 const usersController = require("./controllers/usersController");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use("/api", apiRouter); //if the endpoint is ./api then go to the apiRouter.js file
 module.exports = app;
