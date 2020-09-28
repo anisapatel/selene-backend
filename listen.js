755e082d66676a2d03721adec3b81d04a29f33ae
anisapatel <anisa_patel@live.co.uk>
2020-09-28 14:58:01 +0100

    setting up to push to heroku

--- listen.js
+++ listen.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = 4000;
+const port = process.env.PORT || 3000;
 
 //ensures server is listening to incoming requests from the client side
 //binds app to the relevant port
