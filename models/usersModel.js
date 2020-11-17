df58f55f094fccbd7e41000a123513e9de6ca207
anisapatel <anisa_patel@live.co.uk>
2020-11-17 17:44:59 +0000

    removed auth0 provider

--- models/usersModel.js
+++ models/usersModel.js
@@ -9,7 +9,6 @@ const user = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   username: { type: String, required: true },
-  auth0_id: { type: String, required: true },
   profile_picture: { type: String },
 });
 
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,13 +4,10 @@ const apiRouter = express.Router();
 const usersRouter = require("./usersRouter");
 const productsRouter = require("./productsRouter");
 const adminRouter = require("./adminRouter");
-const { login, logout } = require("../controllers/usersController");
 const app = express.Router();
 
 apiRouter.use("/user-data", usersRouter);
 apiRouter.use("/products", productsRouter);
 apiRouter.use("/users", adminRouter);
-app.route("/auth/callback").get(login);
-apiRouter.route("/logout").post(logout);
 
 module.exports = apiRouter;
